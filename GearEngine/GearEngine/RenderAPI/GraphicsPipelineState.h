edd9369d1bce4763e6487475342ed5402ff7b8e0
PYZ <592744797@qq.com>
2019-07-06 16:53:32 +0800

    [feature]:重新修改

---
+++ GearEngine/GearEngine/RenderAPI/GraphicsPipelineState.h
@@ -0,0 +1,15 @@
+#ifndef GRAPHICS_PIPELINE_STATE
+#define GRAPHICS_PIPELINE_STATE
+#include "VulkanContext.h"
+#include <map>
+class GraphicsPipelineState
+{
+public:
+	GraphicsPipelineState() {}
+	~GraphicsPipelineState() {}
+
+private:
+	//vertex desc
+};
+#endif
+
