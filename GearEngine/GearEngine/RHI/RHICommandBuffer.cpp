d7ccbf84eb8d71fdc034bb57114517a1cbb906aa
PYZ <592744797@qq.com>
2019-08-04 22:45:05 +0800

    [feature]:为每个cmd buffer分配一个fence

--- GearEngine/GearEngine/RHI/RHICommandBuffer.cpp
+++ GearEngine/GearEngine/RHI/RHICommandBuffer.cpp
@@ -4,10 +4,21 @@
 RHICommandBuffer::RHICommandBuffer(RHIDevice* device)
 	:mDevice(device)
 {
+	//分配fence
+	//todo:后续将创建fence步骤转移至device上
+	VkFenceCreateInfo fenceInfo = {};
+	fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+	fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+	
+	if (vkCreateFence(mDevice->getDevice(), &fenceInfo, nullptr, &mFence) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create fence!");
+	}
 }
 
 RHICommandBuffer::~RHICommandBuffer()
 {
+	vkDestroyFence(mDevice->getDevice(), mFence, nullptr);
 	vkFreeCommandBuffers(mDevice->getDevice(), mCommandPool, 1, &mCommandBuffer);
 }
 
