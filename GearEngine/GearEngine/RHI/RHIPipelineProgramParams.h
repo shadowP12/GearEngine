85f7515fd75da848557cc79c530d1f0c0f5186c3
PYZ <592744797@qq.com>
2019-08-07 22:39:19 +0800

    [feature]:补充一些常用结构

--- GearEngine/GearEngine/RHI/RHIPipelineProgramParams.h
+++ GearEngine/GearEngine/RHI/RHIPipelineProgramParams.h
@@ -1,7 +1,8 @@
 #ifndef RHI_PIPELINR_PROGRAM_PARAMS
 #define RHI_PIPELINE_PROGRAM_PARAMS
 #include "RHIDefine.h"
-#include "RHIProgramParameter.h"
+#include "RHIProgramParam.h"
+
 
 
 #endif
--- GearEngine/GearEngine/RHI/RHIProgramParam.h
+++ GearEngine/GearEngine/RHI/RHIProgramParam.h
@@ -1,5 +1,85 @@
 #ifndef RHI_PROGRAM_PARAM
 #define RHI_PROGRAM_PARAM
 
+#include "RHIDefine.h"
+#include <vector>
+#include <string>
 
+enum class RHIParamDataType
+{
+	FLOAT1 = 1,
+	FLOAT2 = 2,
+	FLOAT3 = 3,
+	FLOAT4 = 4,
+	MATRIX_2X2 = 11,
+	MATRIX_2X3 = 12,
+	MATRIX_2X4 = 13,
+	MATRIX_3X2 = 14,
+	MATRIX_3X3 = 15,
+	MATRIX_3X4 = 16,
+	MATRIX_4X2 = 17,
+	MATRIX_4X3 = 18,
+	MATRIX_4X4 = 19,
+	INT1 = 20,
+	INT2 = 21,
+	INT3 = 22,
+	INT4 = 23,
+	BOOL = 24,
+	STRUCT = 25,
+	COLOR = 26,
+	COUNT = 27,
+	UNKNOWN = 0xffff
+};
+
+// note:不完善但基本够用
+enum RHIParamObjectType
+{
+	SAMPLER1D = 1,
+	SAMPLER2D = 2,
+	SAMPLER3D = 3,
+	SAMPLERCUBE = 4,
+	TEXTURE1D = 11,
+	TEXTURE2D = 12,
+	TEXTURE3D = 13,
+	TEXTURECUBE = 14,
+	TEXTURE1DARRAY = 21,
+	TEXTURE2DARRAY = 22,
+	TEXTURE3DARRAY = 23,
+	TEXTURECUBEARRAY = 24,
+	UNKNOWN = 0xffff
+};
+
+// note:
+// 在vulkan的shader中不能直接使用单一数据,只能使用uniform buffer(以下使用ParamBlock表示)
+struct RHIParamDataInfo
+{
+	std::string name;
+	RHIParamDataType type;
+	UINT32 elementSize;
+	UINT32 arraySize;
+	UINT32 arrayElementStride;
+	UINT32 paramBlockSlot;
+	UINT32 paramBlockSet;
+	//shader里面的数据会自动优化成4的倍数,所以内存的偏移不能直接按照数据类型去进行计算
+	UINT32 memOffset;
+};
+
+struct RHIParamObjectInfo
+{
+	std::string name;
+	RHIParamObjectType type;
+	UINT32 slot;
+	UINT32 set;
+};
+
+struct RHIParamBlockInfo
+{
+	std::string name;
+	UINT32 slot;
+	UINT32 set;
+	//必须为4的倍数
+	UINT32 blockSize;
+	//是否能让不同管线阶段共享
+	bool isShareable;
+};
 #endif
