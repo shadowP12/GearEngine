1911820caf2e43a9b55d0bdd25e97e397d08f49e
PYZ <592744797@qq.com>
2019-08-13 00:33:03 +0800

    [feature]:补充pipeline state

--- GearEngine/GearEngine/RHI/RHIPipelineState.cpp
+++ GearEngine/GearEngine/RHI/RHIPipelineState.cpp
@@ -1,1 +1,38 @@
 #include "RHIPipelineState.h"
+
+RHIGraphicsPipelineState::VariantKey::VariantKey(uint32_t inRenderpassID)
+	:renderpassID(inRenderpassID)
+{
+}
+
+size_t RHIGraphicsPipelineState::VariantKey::HashFunction::operator()(const VariantKey& v) const
+{
+	size_t hash = 0;
+	Hash(hash, v.renderpassID);
+
+	return hash;
+}
+
+bool RHIGraphicsPipelineState::VariantKey::EqualFunction::operator()(
+	const VariantKey& lhs, const VariantKey& rhs) const
+{
+	if (lhs.renderpassID != rhs.renderpassID)
+		return false;
+
+	return true;
+}
+
+RHIGraphicsPipelineState::RHIGraphicsPipelineState(RHIDevice* device, const RHIPipelineStateInfo& info)
+	:mDevice(device), mVertexProgram(info.vertexProgram), mFragmentProgram(info.fragmentProgram)
+{
+
+}
+
+RHIGraphicsPipelineState::~RHIGraphicsPipelineState()
+{
+}
+
+VkPipeline RHIGraphicsPipelineState::getPipeline()
+{
+	return VkPipeline();
+}
--- GearEngine/GearEngine/RHI/RHIPipelineState.h
+++ GearEngine/GearEngine/RHI/RHIPipelineState.h
@@ -1,4 +1,58 @@
 #ifndef RHI_PIPELINE_STATE_H
 #define RHI_PIPELINE_STATE_H
+#include "RHIDefine.h"
+#include "RHIProgram.h"
+#include "RHIProgramParam.h"
+#include "Utility/Hash.h"
+
+// note:暂不加入计算管线
+// note:使用智能指针来传参会好一点?
+
+struct RHIPipelineStateInfo
+{
+	//programs
+	RHIProgram* vertexProgram = nullptr;
+	RHIProgram* fragmentProgram = nullptr;
+
+	//todo:render states
+};
+
+class RHIDevice;
+
+class RHIGraphicsPipelineState
+{
+public:
+	RHIGraphicsPipelineState(RHIDevice* device, const RHIPipelineStateInfo& info);
+	~RHIGraphicsPipelineState();
+	VkPipeline getPipeline();
+private:
+	struct VariantKey
+	{
+		// note:开发阶段仅使用render pass生成hash key,后续还有绘制方式以及顶点输入布局
+		VariantKey(uint32_t inRenderpassID);
+
+		class HashFunction
+		{
+		public:
+			std::size_t operator()(const VariantKey& key) const;
+		};
+
+		class EqualFunction
+		{
+		public:
+			bool operator()(const VariantKey& lhs, const VariantKey& rhs) const;
+		};
+
+		uint32_t renderpassID;
+	};
+
+
+private:
+	RHIDevice* mDevice;
+	RHIProgram* mVertexProgram = nullptr;
+	RHIProgram* mFragmentProgram = nullptr;
+
+	VkPipeline mPipeline;
+};
 
 #endif
