bb94ece0988c1daf199f685841f1eccf0aab3189
pan <592744797@qq.com>
2021-03-13 16:47:48 +0800

    [feature]:接入三方库

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-#include <Engine/Utility/FileSystem.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 
 int main()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GearEditor", nullptr, nullptr);
+    void* windowPtr = glfwGetWin32Window(window);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
